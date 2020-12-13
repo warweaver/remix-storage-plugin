a7aeb121efeabb2ff4850d1ca18bd024afbff228
Your Name <you@example.com>
2020-12-13 09:12:32 +0100

    starting import

--- src/components/3box/3boxService.ts
+++ src/components/3box/3boxService.ts
@@ -3,8 +3,18 @@ import { toast } from "react-toastify";
 import { BehaviorSubject } from "rxjs";
 import { gitservice, ipfservice } from "../../App";
 
+export interface boxObject {
+  key: string;
+  cid?: string;
+  datestored?: string | number | Date;
+  datecommit?: number | string | Date;
+  ref?: string;
+  message?: string;
+  links?: string;
+}
 export class BoxService {
-  status = new BehaviorSubject<boolean>(false)
+  status = new BehaviorSubject<boolean>(false);
+
   box: any;
   space: any;
 
@@ -13,11 +23,11 @@ export class BoxService {
   hidespinner() {}
   // 3BOX connection
 
-  async setSpace(space:any){
-    this.space = space
+  async setSpace(space: any) {
+    this.space = space;
   }
 
-  async storeHashIn3Box(space:any) {
+  async storeHashIn3Box(space: any) {
     if (typeof this.space == "undefined") {
       toast.warning("You should connect to 3Box first");
       return false;
@@ -27,14 +37,17 @@ export class BoxService {
     console.log("export 3box", ipfservice.cid, this.space);
     const commits = await gitservice.getCommits();
     let key = `remixhash-${Date.now()}`;
-    await this.space.private.set(key, {
+
+    let ob: boxObject = {
       key: key,
       cid: ipfservice.cid,
       datestored: new Date(Date.now()),
       datecommit: commits[0].commit.committer.timestamp,
       ref: commits[0].oid,
       message: commits[0].commit.message,
-    });
+    };
+
+    await this.space.private.set(key, ob);
     toast.success("stored in 3box");
     //this.addSuccess('boxexportstatus', 'Your data was stored in 3Box')
     const hashes = await this.getHashesFrom3Box(space);
@@ -58,7 +71,7 @@ export class BoxService {
     hashes = hashes.reverse();
   }
 
-  async getHashesFrom3Box(space:any) {
+  async getHashesFrom3Box(space: any) {
     const hashes = await space.private.all();
     console.log(hashes);
     return Object.values(hashes);
@@ -72,7 +85,7 @@ export class BoxService {
     //await this.clone();
   }
 
-  async deleteFrom3Box(args: string, space:any) {
+  async deleteFrom3Box(args: string, space: any) {
     const key = args;
     console.log("key", key);
     this.showspinner();
